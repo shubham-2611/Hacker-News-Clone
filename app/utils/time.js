771d54e4a51b5fdaacb2c3bca1040019b54443ad
Gaurav Kumar <85248165+gauravkumar2817@users.noreply.github.com>
2021-10-12 20:39:32 +0530

    Update time.js
    format time

--- app/utils/time.js
+++ app/utils/time.js
@@ -1,4 +1,5 @@
 export default function formatDate(timestamp) {
+  //this is to format the time into hour and minutes
   return new Date(timestamp * 1000).toLocaleDateString("en-US", {
     hour: "numeric",
     minute: "numeric",

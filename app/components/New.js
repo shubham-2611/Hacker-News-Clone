9657dfd0ce5320331c1f44b0e3921491871af2cb
HariomSingh98 <70706137+HariomSingh98@users.noreply.github.com>
2021-10-12 21:54:56 +0530

    showing new postss
    showing new posts

--- app/components/New.js
+++ app/components/New.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Stories from "./Stories";
 
+//showing new stroies 
 export default class New extends React.Component {
   render() {
     return <Stories type="new" />;
